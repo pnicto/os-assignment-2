fd44442dfab545506caece18fbcc4aec1c6da0e7
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-11-09 15:43:46 +0530

    feat: initial functionality to client

---
+++ include/client.h
@@ -0,0 +1,15 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+
+#include "utils.h"
+
+#endif
